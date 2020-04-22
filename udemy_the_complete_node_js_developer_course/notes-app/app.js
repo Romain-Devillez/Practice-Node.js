ae5f82349ee01b287ece77cf6e24a6981a59031b
Romain <rom.dev@live.fr>
2020-04-22 11:58:37 +0200

    03.12 Printing in Color [DONE]

--- udemy_the_complete_node_js_developer_course/notes-app/app.js
+++ udemy_the_complete_node_js_developer_course/notes-app/app.js
@@ -2,9 +2,14 @@ const validator = require('validator')
 
 const getNotes = require('./notes');
 
+const chalk = require('chalk');
+
+
 const msg = getNotes();
 
 console.log(msg);
+
+console.log(chalk.green.bold.inverse('Success'))
 
 console.log(validator.isEmail('andrew@exemple.com'));
 console.log(validator.isURL('https://mead.io'));
