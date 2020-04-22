bd6b604e65cd19c25b20a858e5c8f8c67bdcb2ab
Romain <rom.dev@live.fr>
2020-04-22 17:48:18 +0200

    04.19 - Adding a Note [DONE]

--- udemy_the_complete_node_js_developer_course/notes_app/app.js
+++ udemy_the_complete_node_js_developer_course/notes_app/app.js
@@ -1,62 +1,56 @@
-const validator = require('validator')
-const getNotes = require('./notes');
-const chalk = require('chalk');
-const yargs = require('yargs');
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
 
-// Customive yargs version
+// Customize yargs version
 yargs.version('1.1.0')
 
 // Create add command
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    builder :
-        {
-            title: {
-                describe: 'Note title',
-                demandOption: true,
-                type: 'string'
-            },
-            body: {
-                describe: 'Add your notes',
-                demandOption: true,
-                type: 'string'
-            }
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
         },
-    handler: function () {
-        console.log('Title: ' + argv.title + 'Description :' + argv.description)
-
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.addNote(argv.title, argv.body)
     }
 })
 
 // Create remove command
 yargs.command({
-    command:  'remove',
+    command: 'remove',
     describe: 'Remove a note',
     handler: function () {
-        console.log('Removing a note !')
-
+        console.log('Removing the note')
     }
 })
 
 // Create list command
 yargs.command({
-    command:  'list',
+    command: 'list',
     describe: 'List your notes',
     handler: function () {
-        console.log('Placeholder')
-
+        console.log('Listing out all notes')
     }
 })
 
 // Create read command
 yargs.command({
-    command:  'read',
+    command: 'read',
     describe: 'Read a note',
     handler: function () {
-        console.log('Placeholder')
-
+        console.log('Reading a note')
     }
 })
 
-console.log(yargs.argv)
+yargs.parse()
