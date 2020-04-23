ec61cda0b12bb82325c034301a4177908f8c6340
Romain <rom.dev@live.fr>
2020-04-23 23:07:28 +0200

    04.32 - Customizing HTTP Requests [DONE]

--- udemy_the_complete_node_js_developer_course/weather_app/app.js
+++ udemy_the_complete_node_js_developer_course/weather_app/app.js
@@ -1,2 +1,10 @@
 const request = require('request')
 
+const url = 'http://api.weatherstack.com/current?access_key=8119a0fabab1dc3adb7e40c1c2fce55d&query=New%20York'
+
+request({url: url, json: true}, (error, response) => {
+    const current = response.body.current
+
+    console.log
+        (current.weather_descriptions + ". It is currently " + current.temperature + " degrees out. It feels like " + current.feelslike + " degrees out.")
+})
