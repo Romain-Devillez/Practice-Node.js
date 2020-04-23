03628dd775da02b1c7273b552ff3e69a94e59b16
Romain <rom.dev@live.fr>
2020-04-23 16:41:23 +0200

    04.22 - Refactoring tu use Arrow functions [DONE]

--- udemy_the_complete_node_js_developer_course/notes_app/notes.js
+++ udemy_the_complete_node_js_developer_course/notes_app/notes.js
@@ -2,19 +2,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-const getNotes = function ()
-{
-    return "Your notes ..."
-}
+const getNotes = () => "Your notes ..."
 
-const addNote = function(title, body)
+const addNote = (title, body) =>
 {
     const notes = loadNotes()
 
-    const duplicateNotes = notes.filter(function (note) {
-        return note.title === title
-    })
-
+    const duplicateNotes = notes.filter((note) => note.title === title)
 
    if (duplicateNotes.length === 0)
    {
@@ -32,14 +26,11 @@ const addNote = function(title, body)
 
 }
 
-const removeNote = function (title)
+const removeNote = (title) =>
 {
     const notes = loadNotes()
 
-    const notesToKeep = notes.filter(function (note)
-    {
-        return note.title !== title
-    })
+    const notesToKeep = notes.filter((note) => note.title !== title )
 
     if (notesToKeep.length === notes.length)
     {
@@ -54,7 +45,7 @@ const removeNote = function (title)
 }
 
 // Charge le contenu JSON et l'intéprête
-const loadNotes = function()
+const loadNotes = () =>
 {
     try
     {
@@ -70,7 +61,7 @@ const loadNotes = function()
     
 }
 
-const saveNotes = function(notes)
+const saveNotes = (notes) =>
 {
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
