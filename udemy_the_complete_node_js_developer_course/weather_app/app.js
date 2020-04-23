87badc5772f9071ed6fad8f6f10b2dd5d61a5fd5
Romain <rom.dev@live.fr>
2020-04-23 22:29:32 +0200

    04.31 - Making Http request [DONE]

--- udemy_the_complete_node_js_developer_course/weather_app/app.js
+++ udemy_the_complete_node_js_developer_course/weather_app/app.js
@@ -1,12 +1,2 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout(() => {
-    console.log('2 second Timer')
-}, 2000)
-
-setTimeout(() => {
-    console.log('0 second Timer')
-}, 0)
-
-
-console.log('Stopping')
