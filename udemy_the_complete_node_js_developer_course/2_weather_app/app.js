d2437aa131885f8430b5403e460fda4d5c918ebd
Romain <rom.dev@live.fr>
2020-04-27 11:48:15 +0200

    04.38 -  Callback Chaining [DONE]

--- udemy_the_complete_node_js_developer_course/2_weather_app/app.js
+++ udemy_the_complete_node_js_developer_course/2_weather_app/app.js
@@ -1,14 +1,37 @@
 const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require ('./utils/forecast')
+const readline = require('readline');
+
+const address = process.argv[2]
+
+if(!address){
+    console.log('Please provide an address')
+} else {
+
+    geocode(address, (error, data) => {
+
+        if (error){
+            return console.log(error)
+        }
+
+        console.log('Error', error)
+        console.log('Data', data)
+
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+
+            if(error){
+                return console.log(error)
+            }
+
+            console.log(data.location)
+            console.log(forecastData)
+
+        })
+    })
+
+}
 
 
-geocode('Boston', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+
--- udemy_the_complete_node_js_developer_course/2_weather_app/utils/forecast.js
+++ udemy_the_complete_node_js_developer_course/2_weather_app/utils/forecast.js
@@ -7,12 +7,12 @@ const forecast = (longitude, latitude, callback) => {
     {
         if (error) {
             callback('Unable to connect to location services!', undefined)
-        } else if (response.body.features.length === 0) {
-            callback('Unable to find location. Try another search.', undefined)
+        } else if (response.body.message) {
+            callback(console.log(response.body.message))
         } else {
             callback(undefined, {
-                latitude: response.body.features[0].center[0],
-                longitude: response.body.features[0].center[1],
+                latitude: response.body.features[0].center[1],
+                longitude: response.body.features[0].center[0],
                 location: response.body.features[0].place_name
             })
         }
