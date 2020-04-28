bda52b3b12b0d5974a259974af3f71df874cc4b3
Romain <rom.dev@live.fr>
2020-04-28 12:15:10 +0200

    04.47 - Dynamic Page with Templating [DONE]

--- udemy_the_complete_node_js_developer_course/3_web_servers/src/app.js
+++ udemy_the_complete_node_js_developer_course/3_web_servers/src/app.js
@@ -4,12 +4,35 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Set express for template handlebars version
+app.set('view engine', 'hbs')
+
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+        res.render('index', {
+            title: 'Weather App',
+            name: 'Andrew Mead'
+        })
+    })
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About page',
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
         forecast: '',
         location: ''
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'help page',
+        helper: 'You nedd help ?'
     })
 })
 
