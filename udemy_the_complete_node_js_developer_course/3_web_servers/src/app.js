ed0e587fa190b1d2ae0aba3015fff9eb0c57513b
Romain <rom.dev@live.fr>
2020-04-28 15:54:57 +0200

    04.49 - Advance Templating [DONE]

--- udemy_the_complete_node_js_developer_course/3_web_servers/src/app.js
+++ udemy_the_complete_node_js_developer_course/3_web_servers/src/app.js
@@ -1,44 +1,50 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewPath = path.join(__dirname, '../templates')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Set express for templates handlebars version
 app.set('view engine', 'hbs')
 // Set express for custome view directory
 app.set('views', viewPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
         res.render('index', {
-            title: 'Weather App',
-            name: 'Andrew Mead'
+            title: 'Home page',
+            name: 'Romain'
         })
     })
 
 app.get('/about', (req, res) => {
-    res.render('about', {
+    res.render('About page', {
         title: 'About page',
+        name: 'Romain'
     })
 })
 
 app.get('/weather', (req, res) => {
     res.send({
+        title: 'Weather page',
         forecast: '',
-        location: ''
+        location: '',
+        name: 'Romain'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        title: 'help page',
-        helper: 'You nedd help ?'
+        title: 'Help page',
+        name: 'Romain'
     })
 })
 
