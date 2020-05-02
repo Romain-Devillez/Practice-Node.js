60d0eb8a65efcfab91fb5037c932d4c61c978769
Romain <rom.dev@live.fr>
2020-05-02 13:26:12 +0200

    04.78 - Querying Documents [DONE]

--- udemy_the_complete_node_js_developer_course/4_task_app/mongodb.js
+++ udemy_the_complete_node_js_developer_course/4_task_app/mongodb.js
@@ -4,10 +4,6 @@ const { MongoClient, ObjectID} = require('mongodb')
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectID()
-console.log(id)
-console.log(id.getTimestamp())
-
 MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error, client) => {
     if (error) {
         console.log('Unable to connect to database')
@@ -17,32 +13,15 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error, client) => 
 
     const db = client.db(databaseName)
 
-    // db.collection('users').insertOne({
-    //     name: 'Romain',
-    //     age: 26
-    // }, (error, result) => {
-    //     if (error) {
-    //         console.log('Unable to insert User')
-    //     }
-    //
-    //     console.log(result.ops)
-    // })
-
-    db.collection('users').insertMany([
-        {
-            name: 'Julien',
-            age: 29
-        },
-        {
-            name: 'Loetitia',
-            age: 45
-        }
-
-    ], (error, result) => {
+    db.collection('users').findOne({ name: 'Romain'}, (error, user) => {
         if (error) {
-            console.log('Unable to insert documents')
+            return console.log('Unable to Fetch')
         }
 
-        console.log(result.ops)
+        console.log(user)
+    })
+
+    db.collection('users').find({age: 26}).toArray((error, users) => {
+        console.log(users)
     })
 })
