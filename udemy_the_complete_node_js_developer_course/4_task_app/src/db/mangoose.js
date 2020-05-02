226ce55e505b3a3ca2478e34164002b35cae1ce1
Romain <rom.dev@live.fr>
2020-05-02 17:31:37 +0200

    04.89 - Ressource Creation Endpoint part 1 [DONE]

--- udemy_the_complete_node_js_developer_course/4_task_app/src/db/mangoose.js
+++ udemy_the_complete_node_js_developer_course/4_task_app/src/db/mangoose.js
@@ -1,61 +1,8 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
-})
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if(!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if(value < 0) {
-                throw new Error('Age must be a positive number')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        tim: true,
-        minlength: 7,
-        validate(value) {
-            if(value.toLocaleLowerCase().includes('password')) {
-                throw new Error('Password cannot contain "password" ')
-            }
-        }
-    }
-
-})
-
-const me = new User({
-    name: " Julien",
-    email: 'ju.DEV@live.fr',
-    password: 'bonjourbonjour '
-})
-
-me.save().then( () => {
-    console.log(me)
-}).catch( (error) => {
-    console.log('error', error)
 })
 
 const Task = mongoose.model('Task', {
@@ -71,15 +18,6 @@ const Task = mongoose.model('Task', {
     }
 })
 
-const task = new Task( {
-    description: 'Learn the Mongoose library'
-})
-
-task.save().then( () => {
-    console.log(me)
-}).catch( (error) => {
-    console.log('error', error)
-})
 
 
 
