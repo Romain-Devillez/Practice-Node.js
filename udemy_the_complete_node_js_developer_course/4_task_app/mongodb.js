fd8b60cc10db4785ce865fe9756d4e875db17d08
Romain <rom.dev@live.fr>
2020-05-02 13:10:00 +0200

    04.77 - Object ID [DONE]

--- udemy_the_complete_node_js_developer_course/4_task_app/mongodb.js
+++ udemy_the_complete_node_js_developer_course/4_task_app/mongodb.js
@@ -1,9 +1,12 @@
-const mongodb = require('mongodb')
 
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectID} = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID()
+console.log(id)
+console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error, client) => {
     if (error) {
