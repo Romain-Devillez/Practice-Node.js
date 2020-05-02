04422e04a6fbeccf8f1388b5735569075a1ebccc
Romain <rom.dev@live.fr>
2020-05-02 16:40:57 +0200

    04.84 - Data Validation and Sanitization part 1 [DONE]

--- udemy_the_complete_node_js_developer_course/4_task_app/src/db/mangoose.js
+++ udemy_the_complete_node_js_developer_course/4_task_app/src/db/mangoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -7,16 +8,35 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if(value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
 })
 
 const me = new User({
-    name: "Romain",
-    age: 26
+    name: " Julien",
+    email: 'ju.DEV@live.fr'
 })
 
 me.save().then( () => {
