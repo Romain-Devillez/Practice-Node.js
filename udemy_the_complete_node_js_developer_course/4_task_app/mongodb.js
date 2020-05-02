bbfe753564441905989c98b9bae157420d1834c1
Romain <rom.dev@live.fr>
2020-05-02 12:39:47 +0200

    04.75 - Connecting & Inserting Documents [DONE]

---
+++ udemy_the_complete_node_js_developer_course/4_task_app/mongodb.js
@@ -0,0 +1,21 @@
+const mongodb = require('mongodb')
+
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error, client) => {
+    if (error) {
+        console.log('Unable to connect to database')
+    }
+
+    console.log('Connect ok')
+
+    const db = client.db(databaseName)
+
+    db.collection('users').insertOne({
+        name: 'Romain',
+        age: 26
+    })
+})
