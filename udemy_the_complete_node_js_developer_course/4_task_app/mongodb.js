2dbeb38d9d20c9a57c780a16198f07c9442141ec
Romain <rom.dev@live.fr>
2020-05-02 12:53:10 +0200

    04.76 - Inserting Document [DONE]

--- udemy_the_complete_node_js_developer_course/4_task_app/mongodb.js
+++ udemy_the_complete_node_js_developer_course/4_task_app/mongodb.js
@@ -14,8 +14,32 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error, client) => 
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Romain',
-        age: 26
+    // db.collection('users').insertOne({
+    //     name: 'Romain',
+    //     age: 26
+    // }, (error, result) => {
+    //     if (error) {
+    //         console.log('Unable to insert User')
+    //     }
+    //
+    //     console.log(result.ops)
+    // })
+
+    db.collection('users').insertMany([
+        {
+            name: 'Julien',
+            age: 29
+        },
+        {
+            name: 'Loetitia',
+            age: 45
+        }
+
+    ], (error, result) => {
+        if (error) {
+            console.log('Unable to insert documents')
+        }
+
+        console.log(result.ops)
     })
 })
