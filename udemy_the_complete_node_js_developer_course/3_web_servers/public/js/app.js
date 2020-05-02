70d63c80306fd3a46a3e1e707b1742b4c2332596
Romain <rom.dev@live.fr>
2020-05-02 11:08:50 +0200

    04.59 - Writing up the User Interface [DONE]

--- udemy_the_complete_node_js_developer_course/3_web_servers/public/js/app.js
+++ udemy_the_complete_node_js_developer_course/3_web_servers/public/js/app.js
@@ -2,6 +2,10 @@ console.log('I\'m JS')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+messageOne.textContent = ''
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -9,13 +13,20 @@ weatherForm.addEventListener('submit', (e) => {
     const location = search.value
     console.log(location)
 
+    messageOne.textContent = 'Loading...'
+
     fetch('http://localhost:3000/weather?address=' + location).then( (response) => {
         response.json().then( (data) => {
             if (data.error) {
                 console.log(data.error)
+                messageOne.textContent = data.error
+
             } else {
                 console.log(data.location)
                 console.log(data.forecast)
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast.location
+
             }
         })
     })
