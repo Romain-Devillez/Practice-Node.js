1887338aca8e964a40127dcf14a0075aa1d4ca72
Romain <rom.dev@live.fr>
2020-05-02 17:43:06 +0200

    04.89 - Ressource Creation Endpoint part 2 [DONE]

--- udemy_the_complete_node_js_developer_course/4_task_app/src/index.js
+++ udemy_the_complete_node_js_developer_course/4_task_app/src/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 require('./db/mangoose')
+
 const User = require('./models/user')
+const Task = require('./models/task')
+
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,7 +15,17 @@ app.post('/users', (req, res) => {
     const user = new User(req.body)
 
     user.save().then( () => {
-        res.send(user)
+        res.status(201).send(user)
+    }).catch( (e) => {
+        res.status(400).send(e)
+    })
+})
+
+app.post('/task', (req, res) => {
+    const task = new Task(req.body)
+
+    task.save().then( () => {
+        res.status(201).send(task)
     }).catch( (e) => {
         res.status(400).send(e)
     })
