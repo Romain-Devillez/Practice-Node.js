c4d855e384d8ed44ad3ea3f13c1b1106d0144354
Romain <rom.dev@live.fr>
2020-05-03 12:55:29 +0200

    04.96 - Async/Await part 2 [DONE]

--- udemy_the_complete_node_js_developer_course/4_task_app/playground/promiseChaining.js
+++ udemy_the_complete_node_js_developer_course/4_task_app/playground/promiseChaining.js
@@ -4,20 +4,44 @@ const  Task = require('../src/models/task')
 
 
 
-User.findByIdAndUpdate('5ead87b0cd972e2340f44bf2', { age: 1}).then( (user) => {
-    console.log(user)
-    return User.countDocuments({ age: 1})
-}).then( (result) => {
-    console.log(result)
-}).catch( (e) => {
-    console.log(e)
-})
+// User.findByIdAndUpdate('5ead87b0cd972e2340f44bf2', { age: 1}).then( (user) => {
+//     console.log(user)
+//     return User.countDocuments({ age: 1})
+// }).then( (result) => {
+//     console.log(result)
+// }).catch( (e) => {
+//     console.log(e)
+// })
+//
+// Task.findByIdAndRemove('5ead89e9ee46fa24630a43d3').then( (task) => {
+//     console.log(task)
+//     return Task.countDocuments({ completed: false})
+// }).then( (result) => {
+//     console.log(result)
+// }).catch( (e) => {
+//     console.log(e)
+// })
 
-Task.findByIdAndRemove('5ead89e9ee46fa24630a43d3').then( (task) => {
-    console.log(task)
-    return Task.countDocuments({ completed: false})
-}).then( (result) => {
-    console.log(result)
+// const updateAgeAndCount = async (id, age) => {
+//     const user = await User.findByIdAndUpdate(id,{ age })
+//     const count = await User.countDocuments({ age })
+//     return  count
+// }
+//
+// updateAgeAndCount('5ead87b0cd972e2340f44bf2', 1).then( (count) => {
+//     console.log(count)
+// }).catch( (e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndRemove(id)
+    const count = await Task.countDocuments({ completed: false})
+    return count
+}
+
+deleteTaskAndCount('5ead94d7ddab6a2bdb2c0e10').then( (count) => {
+    console.log(count)
 }).catch( (e) => {
     console.log(e)
 })
