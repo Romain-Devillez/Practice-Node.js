c52f13ee65c1d7d620ee1d6ec28b35a6e5bbfdd3
Romain <rom.dev@live.fr>
2020-05-03 16:49:54 +0200

    04.100 - Ressource Delting Endpoint [DONE]

--- udemy_the_complete_node_js_developer_course/4_task_app/src/index.js
+++ udemy_the_complete_node_js_developer_course/4_task_app/src/index.js
@@ -71,6 +71,20 @@ app.patch('/users/:id', async (req, res) => {
     }
 })
 
+// DELETE route for delete One User
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if(!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
 // GET route for show All Tasks
 app.get('/tasks', async (req, res) => {
 
@@ -132,6 +146,19 @@ app.patch('/tasks/:id', async (req, res) => {
     }
 })
 
+// DELETE route for delete One Task
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if(!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
 
 app.listen(port, () => {
     console.log('Server is up on port : ' + port)
