8c54c9fc446e6fc353c5197fcd3e3ccb6d2762a3
Romain <rom.dev@live.fr>
2020-05-03 12:23:10 +0200

    04.94 - Promise Chaining Challenge [DONE]

--- udemy_the_complete_node_js_developer_course/4_task_app/playground/promiseChaining.js
+++ udemy_the_complete_node_js_developer_course/4_task_app/playground/promiseChaining.js
@@ -1,10 +1,21 @@
 require('../src/db/mangoose')
 const  User = require('../src/models/user')
+const  Task = require('../src/models/task')
+
 
 
 User.findByIdAndUpdate('5ead87b0cd972e2340f44bf2', { age: 1}).then( (user) => {
     console.log(user)
     return User.countDocuments({ age: 1})
+}).then( (result) => {
+    console.log(result)
+}).catch( (e) => {
+    console.log(e)
+})
+
+Task.findByIdAndRemove('5ead89e9ee46fa24630a43d3').then( (task) => {
+    console.log(task)
+    return Task.countDocuments({ completed: false})
 }).then( (result) => {
     console.log(result)
 }).catch( (e) => {
