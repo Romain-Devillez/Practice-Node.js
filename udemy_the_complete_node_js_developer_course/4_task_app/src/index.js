c3e743595b7930907f8420a3af2ca29ad4d7540a
Romain <rom.dev@live.fr>
2020-05-03 11:40:09 +0200

    04.91 - Ressource Reading Endpoint part 1 [DONE]

--- udemy_the_complete_node_js_developer_course/4_task_app/src/index.js
+++ udemy_the_complete_node_js_developer_course/4_task_app/src/index.js
@@ -11,6 +11,30 @@ const port = process.env.PORT || 3000
 // Parse JSON
 app.use(express.json())
 
+// Get route for show All Users
+app.get('/users', (req, res) => {
+    User.find({}).then( (users) => {
+        res.status(200).send(users)
+    }).catch( () => {
+        res.status(500).send()
+    })
+})
+
+// Get route for show One User with Id params
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then( (user) => {
+        if (!user) {
+            return res.status(400).send()
+        }
+        res.send(user)
+    }).catch( () => {
+        res.status(500).send()
+    })
+})
+
+// Post route for Insert User
 app.post('/users', (req, res) => {
     const user = new User(req.body)
 
@@ -21,6 +45,7 @@ app.post('/users', (req, res) => {
     })
 })
 
+// Post route for Insert Task
 app.post('/task', (req, res) => {
     const task = new Task(req.body)
 
@@ -30,6 +55,8 @@ app.post('/task', (req, res) => {
         res.status(400).send(e)
     })
 })
+
+
 
 app.listen(port, () => {
     console.log('Server is up on port : ' + port)
