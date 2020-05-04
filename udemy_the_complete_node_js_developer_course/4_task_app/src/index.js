821e99804d36ab94bc787ae04fd56888c0fa5e80
Romain <rom.dev@live.fr>
2020-05-04 18:48:02 +0200

    04.104 - Securely Storing Password part 2 [DONE]

--- udemy_the_complete_node_js_developer_course/4_task_app/src/index.js
+++ udemy_the_complete_node_js_developer_course/4_task_app/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 require('./db/mangoose')
 
+// Model for database
 const User = require('./models/user')
 const Task = require('./models/task')
 
+// Router
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
@@ -14,6 +16,7 @@ const port = process.env.PORT || 3000
 // Parse JSON
 app.use(express.json())
 
+// File Route
 app.use(userRouter)
 app.use(taskRouter)
 
