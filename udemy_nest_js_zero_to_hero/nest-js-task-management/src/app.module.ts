aaf75ea295ba48f0439acde867c093fddddbddf4
Romain <rom.dev@live.fr>
2020-05-09 17:26:42 +0200

    Section 2 [DONE]

--- udemy_nest_js_zero_to_hero/nest-js-task-management/src/app.module.ts
+++ udemy_nest_js_zero_to_hero/nest-js-task-management/src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 
+
 @Module({
-  imports: [TasksModule],
+  imports:
+    [
+      TasksModule
+    ]
 })
+
 export class AppModule {}
