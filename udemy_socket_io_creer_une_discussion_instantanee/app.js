66583a73e8655f84accb0702e2cf6051aaca2bfb
Romain <rom.dev@live.fr>
2020-05-11 16:20:12 +0200

    Section 3 [DONE]

--- udemy_socket_io_creer_une_discussion_instantanee/app.js
+++ udemy_socket_io_creer_une_discussion_instantanee/app.js
@@ -1,14 +1,19 @@
 // Modules
-const express = require('express');
-const morgan = require('morgan');
-const config = require('./config');
+const express  = require('express');
+const http     = require('http');
+const socketio = require('socket.io');
+const morgan   = require('morgan');
+const config   = require('./config');
 
 // Variables globales
-const app = express();
-let port = config.express.port;
+const app    = express();
+const server = http.Server(app);
+const io     = socketio(server);
+const port     = config.express.port;
 const options = {
     root: __dirname + '/views'
 }
+let usernames = [];
 
 // Middlewares
 app.use(morgan('dev'));
@@ -24,9 +29,53 @@ app.get('/home', (req, res) => {
 })
 
 app.get('/params/:name', (req, res) => {
-    res.send(req.params.name);
+    res.send(req.params.name)
 })
 
+io.on('connection', function (socket) {
 
-// Start application
-app.listen(port, () => console.log('On port ' + port))
+    console.log('a user connected : ' + socket.id);
+
+    // Traitement pour l'assignation d'un username
+    socket.on('setUsername', (usernameWanted) => {
+
+        // Traitement de la chaine de caractères
+        usernameWanted = usernameWanted.trim()
+
+        // Vérification de l'unicité de l'username
+        let usernameTaken = false
+        for (let socketid in usernames) {
+            if (usernames[socketid] == usernameWanted)
+                usernameTaken = true
+        }
+
+        let timeFakeLoading = 1000
+        setTimeout(() => {
+
+            // Traitement final
+            if (usernameTaken) {
+                socket.emit('rejectUsername', usernameWanted)
+            } else {
+                socket.join('users', () => {
+                    usernames[socket.id] = usernameWanted
+                    socket.emit('acceptUsername', usernameWanted)
+                })
+            }
+
+        }, timeFakeLoading);
+
+    })
+
+    // Déconnexion de l'utilisateur
+    socket.on('disconnect', () => {
+        console.log('disconnected' + socket.id)
+        if (usernames[socket.id]) {
+            delete usernames[socket.id]
+            console.log('username deleted')
+        }
+    })
+
+});
+
+// Lancement de l'application
+server.listen(port, () => console.log('Server started on port ' + port))
